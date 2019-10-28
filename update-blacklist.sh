871a150a963ed3f4f01f6b636f8fa2edc264bc85
Unknown <peter2520@gmail.com>
2019-10-28 13:29:35 -0400

    support ipset older version

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -110,7 +110,8 @@ swap $IPSET_BLACKLIST_NAME $IPSET_TMP_BLACKLIST_NAME
 destroy $IPSET_TMP_BLACKLIST_NAME
 EOF
 
-ipset -file  "$IP_BLACKLIST_RESTORE" restore
+#ipset -file  "$IP_BLACKLIST_RESTORE" restore
+ipset restore < "$IP_BLACKLIST_RESTORE"
 
 if [[ ${VERBOSE:-no} == yes ]]; then
   echo
